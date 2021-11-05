acc67f1f625c2744d39882670c39b55657bc84ee
drel <drel.sohu.com@gmail.com>
2021-11-04 22:25:21 -0400

    feat: add mini game roulette

--- gt/utilities/util.py
+++ gt/utilities/util.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 from collections import defaultdict
 from datetime import datetime
@@ -7,6 +8,20 @@ from PIL import Image
 import pytz
 
 TIME_ZONE = pytz.timezone('Asia/Shanghai')
+
+# Timer copied from https://stackoverflow.com/questions/45419723/python-timer-with-asyncio-coroutine
+class Timer:
+    def __init__(self, timeout, callback, *cb_args):
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job(*cb_args))
+
+    async def _job(self, *cb_args):
+        await asyncio.sleep(self._timeout)
+        await self._callback(*cb_args)
+
+    def cancel(self):
+        self._task.cancel()
 
 # Current time
 def current_time():
